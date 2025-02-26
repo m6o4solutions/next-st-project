225f87935e56c5b6b248ad363f7f37cb0257693c
Michael Owuor <michael.owuor@gmail.com>
2025-02-26 21:53:58 +0300

    feat: enhance layout and styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
 import { ReactNode } from "react";
+import { Comfortaa as FontHeader, Montserrat as FontBody } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
 
 import type { Metadata } from "next";
 
@@ -9,21 +17,28 @@ export const metadata: Metadata = {
 	description: "Starter template for NextJS projects using Tailwind CSS.",
 };
 
+const fontHeader = FontHeader({ subsets: ["latin"], variable: "--font-header" });
+const fontBody = FontBody({ subsets: ["latin"], variable: "--font-body" });
+
 const RootLayout = async ({ children }: Readonly<{ children: ReactNode }>) => {
 	return (
-		<html lang="en" suppressHydrationWarning>
+		<html lang="en" suppressHydrationWarning className={cn(fontHeader.variable, fontBody.variable)}>
 			<head>
 				<link href="/favicon.svg" rel="icon" type="image/svg+xml" />
 			</head>
 
-			<body className="flex h-screen flex-col">
-				
-					<header>Header Goes Here</header>
+			<body className={cn("font-body flex h-screen flex-col")}>
+				<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+					<header>
+						<Header />
+					</header>
 
 					<main>{children}</main>
 
-					<footer className="mt-auto">Footer Goes Here</footer>
-				
+					<footer className="mt-auto">
+						<Footer />
+					</footer>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,29 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Container } from "@/components/container";
+
 const Home = () => {
 	return (
-		<article>
-			<section className="pt-36 pb-36">
-				<h1>Home</h1>
+		<Container>
+			<article>
+				<section className="pt-36 pb-36">
+					<h1 className="text-primary font-header text-4xl font-bold tracking-wide">Next.js Starter Template</h1>
 
-				<p className="my-5">
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, cum reprehenderit. Doloremque corrupti
-					aliquid nihil magnam impedit, quisquam soluta? Expedita et dolorum quo laudantium odio esse, distinctio nulla
-					hic ea.
-				</p>
-			</section>
-		</article>
+					<p className="my-5">
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, cum reprehenderit. Doloremque
+						corrupti aliquid nihil magnam impedit, quisquam soluta? Expedita et dolorum quo laudantium odio esse,
+						distinctio nulla hic ea.
+					</p>
+
+					<Button className="rounded-lg font-semibold uppercase" variant="default" asChild>
+						<Link href="/" target="_blank">
+							Go To Homepage
+						</Link>
+					</Button>
+				</section>
+			</article>
+		</Container>
 	);
 };
 
